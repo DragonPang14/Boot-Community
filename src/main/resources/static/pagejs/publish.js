0b4fb7dc7ce5f43050c4758120095746b3cca771
FiFioo <sevennei@foxmail.com>
2020-02-27 22:19:05 +0800

    添加标签搜索和点击添加标签

--- src/main/resources/static/pagejs/publish.js
+++ src/main/resources/static/pagejs/publish.js
@@ -34,3 +34,51 @@ $(function () {
         console.info(tag);
     });
 });
+
+
+var bind_search_event = function () {
+    var search_input = document.querySelector('#search-tag')
+    search_input.addEventListener('input', function (evt) {
+        var input = evt.target.value
+        var search_input_trim = input.trim()
+        if (search_input_trim && search_input_trim != '') {
+            $('.unselect-tag').hide()
+        }
+        $('.unselect-tag').each(function () {
+            var tag_text = this.text.toLowerCase()
+            if (tag_text.includes(search_input_trim.toLowerCase())) {
+                this.style.display = ''
+            }
+        })
+    })
+}
+
+var template_tag = function (tag_text) {
+    var tag = `
+        <a class="btn btn-light btn-sm tag-input mr-2" data-tag-id="${tag_text}" href="javascript:;">
+            ${tag_text}
+            <span class="ml-2">✗</span>
+        </a>
+    `
+    return tag
+}
+
+var bind_tag_click_event = function() {
+    $('.unselect-tag').click(function (e) {
+        var add_tag_btn = document.querySelector('#dropdown-button')
+        var tag_text = e.target.text
+        var tag = template_tag(tag_text)
+        add_tag_btn.insertAdjacentHTML('beforeBegin', tag)
+        bind_tag_close_event()
+    })
+}
+
+var bind_tag_close_event = function(){
+    $('.ml-2').click(function (e) {
+        e.target.parentNode.remove()
+    })
+}
+
+
+bind_search_event()
+bind_tag_click_event()
