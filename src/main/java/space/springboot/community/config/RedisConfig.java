88ae0b36a1850b7430970927b90de46b5d00e250
dream <GLDZPang@outlook.com>
2020-05-11 22:13:08 +0800

    add redistemplate

--- src/main/java/space/springboot/community/config/RedisConfig.java
+++ src/main/java/space/springboot/community/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -26,7 +27,7 @@ public class RedisConfig {
     public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String,Serializable> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
